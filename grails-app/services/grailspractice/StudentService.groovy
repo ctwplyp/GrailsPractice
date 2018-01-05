deef225b516d936e9c405285047a1165f5fbab63
Sean Laing <sean.laing@sample6.com>
2018-01-05 10:40:12 -0800

    f/Criteria-Api: Create filtering criteria - Created a filter service for the student table

--- grails-app/services/grailspractice/StudentService.groovy
+++ grails-app/services/grailspractice/StudentService.groovy
@@ -4,13 +4,29 @@ import grails.transaction.Transactional
 
 @Transactional
 class StudentService {
-    def count =1
+    List filteredStudent(){
+        return Student.list()
+    }
+
+    List filteredStudentWithLastNameDoeOrReferenceNumLessThen45(){
+        def criteria = Student.createCriteria()
+        def result = criteria.list {
+            or {
+                    eq('lastName','Doe')
+                    lt('referenceNumber',45)
+                }
+            }
+        result
+    }
+
+
     Student createStudent(String lastName, String firstName) {
         Student student = new Student()
         student.lastName = lastName
         student.firstName = firstName
         student.referenceNumber=generateReferenceNumber()
         student.save()
+
         return student
     }
     private String generateReferenceNumber() {
